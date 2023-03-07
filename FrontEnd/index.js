dddd839b95671783077b185c69f989bf7d7ebf97
Tarborar <mathieuseurre84@gmail.com>
2023-03-07 17:24:20 +0100

    Ajout filtre & filtrage

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,7 +1,9 @@
+let photos;
+
 async function chargement(){
     const reponse = await fetch('http://localhost:5678/api/works');
-    let photos = await reponse.json();
-
+    photos = await reponse.json();
+    console.log(photos);
     genererPhotos(photos);
 }
 
@@ -26,3 +28,20 @@ function genererPhotos(photos){
         photoElement.appendChild(textElement);
     }
 }
+
+class Filtreur {
+    constructor(nomClasse, filtre) {
+        this.bouton = document.querySelector(nomClasse);
+        this.filtre = filtre;
+        this.bouton.addEventListener("click", () => {
+            const photosFiltrees = photos.filter(this.filtre);
+            document.querySelector(".gallery").innerHTML = "";
+            genererPhotos(photosFiltrees);
+        });
+    }
+}
+
+const filtrerTous = new Filtreur(".tous", photo => photo.category.name !== null);
+const filtrerObjets = new Filtreur(".objets", photo => photo.category.name === 'Objets');
+const filtrerAppartements = new Filtreur(".appartements", photo => photo.category.name === 'Appartements');
+const filtrerHotels = new Filtreur(".hotels", photo => photo.category.name === 'Hotels & restaurants');
