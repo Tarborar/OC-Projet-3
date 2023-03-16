e29ccbab8c0b437ed0ea21b899bc37547e902d17
Tarborar <mathieuseurre84@gmail.com>
2023-03-16 16:50:36 +0100

    filtre backend & couleur

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,22 +1,27 @@
 let works;
+let categories;
+let filtersName;
 const sectionGallery = document.querySelector(".gallery");
 const portfolio = document.querySelector("#portfolio");
+const filters = document.querySelector(".filtres");
 
 const errorMessage = `
     <p class="messageErreur">
         Il semblerait que les projets soient indisponibles pour le moment !
     </p>
-`
+`;
 
 async function fetchWorks(){
     try{
         const response = await fetch('http://localhost:5678/api/works');
         works = await response.json();
-        console.log("appel du fetch pour récupérer toutes les works");
+        categories = works.map(work => work.category.name); // créé un tableau de tous les noms de catégories
+        filtersName = categories.filter((category, current) => categories.indexOf(category) === current); //vérifie les doublons 
         loadingWorks(works);
+        loadingFilters(filtersName);
     } catch (error){
         console.error("Une erreur est survenue:", error);
-        portfolio.innerHTML = errorMessage;
+        portfolio.innerHTML = errorMessage; //affiche interface d'erreur
     }
 }
 
@@ -33,29 +38,67 @@ function loadingWorks(works){
         sectionGallery.appendChild(photoElement);
         photoElement.appendChild(imageElement);
         photoElement.appendChild(textElement);
+
+        /* 
+        <figure>
+            <img src="" alt="">
+            <figcaption></figcaption>
+        </figure>
+        */
     }
 }
 
-class Filter {
-    constructor(className, filter){
-        this.button = document.querySelector(className);
-        this.filter = filter;
-        this.button.addEventListener("click", async () => {
-            await fetchWorks();
-            const filteredWorks = works.filter(this.filter);
+function loadingFilters(filtersName){
+    const ulFilter = document.createElement("ul");
+    ulFilter.classList.add("rangeFiltre", "fontFiltre");
+    filtersName.unshift("Tous");
+    console.log(filtersName);
+    
+    for (let i=0; i < filtersName.length; i++){
+        const liFilter = document.createElement("li");
+        liFilter.innerText = filtersName[i];
+        liFilter.classList.add ("boutonFiltre");
+
+        ulFilter.appendChild(liFilter);
+
+        /*
+        <ul class="rangeFiltre fontFiltre">
+            <li class="boutonFiltre filtreActive">Tous</li>
+            <li class="boutonFiltre">Objets</li>
+            <li class="boutonFiltre">Appartements</li>
+            <li class="boutonFiltre">Hôtels & restaurants</li>
+        </ul>
+        */
+    }
+    ulFilter.children[0].classList.add("filtreActive"); //filtreActive sur le premier filtre "Tous"
+    filters.prepend(ulFilter);
+
+    const filterButtons = document.querySelectorAll(".boutonFiltre"); //ici parce que la class boutonFiltre n'existe pas encore
+    filterButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const filterValue = button.innerText;
+            console.log(button.innerText);
+            let filteredWorks;
+
+            filterButtons.forEach(b => b.classList.remove("filtreActive"));
+            button.classList.add("filtreActive");
+
+            if (filterValue === "Tous"){
+                filteredWorks = works;
+            } else{
+                filteredWorks = works.filter(work => work.category.name === filterValue);
+            }
+
             sectionGallery.innerHTML = "";
-            loadingWorks(filteredWorks); 
-            console.log(filteredWorks);
+            loadingWorks(filteredWorks);
         });
-    }
+    });    
 }
 
 
 
-const filterTous = new Filter(".tous", photo => photo.category.name !== null);
-const filterObjets = new Filter(".objets", photo => photo.category.name === 'Objets');
-const filterAppartements = new Filter(".appartements", photo => photo.category.name === 'Appartements');
-const filterHotels = new Filter(".hotels", photo => photo.category.name === 'Hotels & restaurants');
+
+
 
 
 
--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,6 +1,9 @@
 const footer = document.querySelector("footer");
+const loginButton = document.querySelector("#login");
+
 
 footer.style.bottom = "0";
+loginButton.style.fontWeight = "600";
 
 
 
